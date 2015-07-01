8da1d77f8c3e9742e799e8930985b8daac32c580
AurelienLavorel <teraflop007>
2015-07-01 19:57:35 +0200

    [update] Opti

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 jQuery(document).ready(function() {
-    jQuery('div.inner.content').css('margin-top', jQuery('div.masthead.clearfix').height()+9);
     jQuery('div.inner.content').css('margin-bottom', jQuery('div.mastfoot').height()+9);
-    jQuery(".fancybox").fancybox();
 
-    var options =
-    {
-        srcNode: 'img',             // grid items (class, node)
-        margin: '20px',             // margin in pixel, default: 0px
-        width: '250px',             // grid item width in pixel, default: 220px
-        max_width: '',              // dynamic gird item width if specified, (pixel)
-        resizable: true,            // re-layout if window resize
-        transition: 'all 0.5s ease' // support transition for CSS3, default: all 0.5s ease
+    if ($('div.gridwall img').length) {
+        jQuery(".fancybox").fancybox();
+
+        var options =
+        {
+            srcNode: 'img',             // grid items (class, node)
+            margin: '20px',             // margin in pixel, default: 0px
+            width: '250px',             // grid item width in pixel, default: 220px
+            max_width: '',              // dynamic gird item width if specified, (pixel)
+            resizable: true,            // re-layout if window resize
+            transition: 'all 0.5s ease' // support transition for CSS3, default: all 0.5s ease
+        }
+        $('div.gridwall').gridify(options);
+    } else {
+        jQuery('div.inner.content').css('margin-top', jQuery('div.masthead.clearfix').height()+9);
     }
-    $('div.gridwall').gridify(options);
 })
